adf7f04af17a992a26164bdc8fd10118b3d78938
Vivek Bhatt <93856336+vivekbhatt07@users.noreply.github.com>
2022-08-06 11:18:44 +0000

    style: chalk

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var readlineSync = require("readline-sync");
 var chalk = require('chalk');
 var score = 0;
 
-var userName = readlineSync.question("Enter your name: ");
-console.log(chalk.blue('Welcome, ' + userName + " to " + "'DO YOU KNOW VIVEK'"));
-console.log("-------------------------");
+var userName = readlineSync.question(chalk.green("Enter your name: "));
+console.log(chalk.yellow("----------------------------------"));
+console.log('Welcome, ' + userName + " to " + chalk.bgMagenta("'DO YOU KNOW VIVEK'"));
+console.log(chalk.yellow("----------------------------------"));
 function play(question, answer) {
   var userAnswer = readlineSync.question(question);
   if (userAnswer === answer) {
     score = score + 1;
-    console.log("Right")
+    console.log(chalk.greenBright("You are Right !"))
   } else {
-    console.log("Wrong")
+    console.log(chalk.red("You are Wrong !"))
   }
   console.log("Your score is: " + score);
-  console.log("-------------------------");
+  console.log(chalk.yellow("----------------------------------"));
 }
 
 var qA = [{ question: "where do I live ? ", answer: "rudrapur" },
